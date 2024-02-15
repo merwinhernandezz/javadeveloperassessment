835b13cb4356bca50b7bcb24258f2deb39e57310
merwinhernandezz <94698033+merwinhernandezz@users.noreply.github.com>
2024-02-15 17:41:08 +0800

    Add files via upload

---
+++ JavaAssessment/src/javaassessment/JavaAssessment.java
@@ -0,0 +1,11 @@
+package javaassessment;
+
+public class JavaAssessment{
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Form().setVisible(true);
+       
+    }
+    
+}
